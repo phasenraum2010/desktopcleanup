a62c120ba5c19fe3026df60ad9c74d630ce34841
phasenraum2010 <thomas@woehlke.org>
2017-12-10 20:35:56 +0100

    initial commit

--- src/main/java/org/woehlke/tools/desktopcleanup/DesktopCleanup.java
+++ src/main/java/org/woehlke/tools/desktopcleanup/DesktopCleanup.java
@@ -4,10 +4,16 @@ import org.woehlke.tools.desktopcleanup.control.DesktopCleanupController;
 
 public class DesktopCleanup {
 
-    private static final String srcDir = "/Users/tw/Downloads";
+    //private static final String srcDir = "/Users/tw/Downloads";
+
+    //private static final String targetDir = "/Users/tw/";
+
+    private static final String srcDir = "/Volumes/WD_2TB_Elements/INBOX";
+
+    private static final String targetDir = "/Volumes/WD_2TB_Elements/archiv";
 
     private static final boolean workDir = true;
-    private static final String targetDir = "/Users/tw/";
+
 
     //private static final String targetDir = "/Users/tw/Desktop/TEST_SORT";
 
